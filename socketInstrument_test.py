78bf0421f79133f9909c55f2cbe5cbfdf360de8f
Helge Stenström <h.stenstrom@gmail.com>
2017-05-21 21:58:10 +0200

    Städa och refactor. Bättre filnamn.

--- socketInstrument_test.py
+++ socketInstrument_test.py
@@ -1,54 +1,35 @@
-import votschSocket
+import socketInstrument
 import unittest
 import unittest.mock
 import sys
 from io import StringIO
+from argparse import ArgumentError
 
 # Idé: jag vill ha ett beteende som kan pluggas i, så att jag lätt kan simulera olika instrument.
 # Lätt byta mellan Vötsch och ett SCPI-instrument.
 # Därmed ändras Vötsch till ett allmänt instrumnt, och olika instrument har olika response-funktioner.
 
-class main_Tests(unittest.TestCase):
-
-    def setUp(self):
-        self.v = votschSocket.vötschBySocket()
-        self.stdout = StringIO()
-        sys.stdout = self.stdout
-
-
-    def tearDown(self):
-        sys.stdout = sys.__stdout__
-
-
-    def testSkriv(self):
-        "Utskrifter kan testas"
-        print("hej")
-
-        self.assertEqual(self.stdout.getvalue().strip(), "hej")
-        self.assertTrue(self.stdout.getvalue().startswith("hej"))
-
-
-    def testPrint(self):
-        command = "$01I"
-        response = self
+class realMain_Tests(unittest.TestCase):
 
     def testMain1(self):
-        #m = votschSocket.main()
-        print("Success")
-        self.assertTrue(self.stdout.getvalue().startswith("Success"))
+
+        sys.argv = ["", "Invalid_argument"]
+        self.assertRaises(SystemExit, socketInstrument.main)
+        # Denna metod failar, om den inte får Vt, Vc eller RotaryDisc som argument.
+        # Annars så returnerar den aldrig. Den måste ha en separat tråd.
 
 
-class response_Tests(unittest.TestCase):
+class vötsch_response_Tests(unittest.TestCase):
 
     def setUp(self):
-        self.v = votschSocket.vötschBySocket()
+        self.v = socketInstrument.vötschBySocket()
 
     def testQuery(self):
         command = "$01I"
         response = self.v.responseFunction(command)
         parts = response.split()
 
-        self.assertEqual(len(parts), 17)
+        self.assertEqual(len(parts), 15)
         binaryParts = parts[-1]
         self.assertEqual(len(binaryParts), 32)
 

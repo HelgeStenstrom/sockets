3b54e9a57aa33bd14130ec0cf55a5b22ca2251aa
Helge Stenström <helge.stenstrom@ericsson.com>
2018-06-27 11:23:36 +0200

    ta bort onödigt test av split/join

--- vc3_test.py
+++ vc3_test.py
@@ -324,21 +324,3 @@ def cmd_for_set(temp, humidity=0.0, fanSpeed=50.0):
     return cmd
 
 
-class otherTests(unittest.TestCase):
-    def test_embedded_eol(self):
-        # Setup
-        longstring = """
-a
-b
-c
-        """
-
-        # Exercise
-        pieces = longstring.splitlines()
-        changed = "\r\n".join(pieces)
-
-        # Verify
-        self.assertIn('a\nb\nc', longstring)
-
-        self.assertIn('a\r\nb\r\nc', changed)
-
